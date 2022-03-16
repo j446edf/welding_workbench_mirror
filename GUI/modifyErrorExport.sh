3c9babc1768bd0b9c13193bec12e27882ff2d4e4
Talha Khan <talha.khan@student.manchester.ac.uk>
2022-03-16 10:24:07 +0000

    [#12]: simplified passing of thermal results database

--- GUI/modifyErrorExport.sh
+++ GUI/modifyErrorExport.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+. ../user.config
+
+export USER_WELDWB_srcDir
+
+USER_INP_RESU_FILE=$resu
+export USER_INP_RESU_FILE
+
+#./modifyErrorExport.sh
 cd $USER_WELDWB_srcDir/postprocessing
-####################################################################################################################
-### Use selected .base
-####################################################################################################################
 sed -i "s@R base.*@R base $USER_INP_RESU_FILE DC 0@g" project.export
+
+
+unset USER_INP_RESU_FILE
